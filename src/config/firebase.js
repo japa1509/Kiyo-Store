b34b9eeed9af5fa53bc5329011275033b2d0dfb6
jesus perez <japa1509@hotmail.com>
2025-01-02 21:09:46 -0500

    Moviendo las credenciales de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAX5FyLPFzzuB4lWne6wqA8F1wBC-aCARU",
-  authDomain: "kiyostore-e0aed.firebaseapp.com",
-  projectId: "kiyostore-e0aed",
-  storageBucket: "kiyostore-e0aed.firebasestorage.app",
-  messagingSenderId: "1018123799375",
-  appId: "1:1018123799375:web:7617312f3580d18c27d33a"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
